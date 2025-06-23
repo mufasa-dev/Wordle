177e569685a1a1cee5b1a0df9b34ff53f5002e85
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-22 23:36:26 -0300

    feat: reajust words

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
   import './styles/TailwindCSS.svelte'
+  import { getRandomWord } from './game/words';
+
+  let palavraSecreta: string;
+  let start: boolean = false;
+
+
+  function  startGame() {
+    palavraSecreta = getRandomWord();
+  }
 </script>
 
 <main>
-  
+  <button>
+    Play
+  </button>
 </main>
-
