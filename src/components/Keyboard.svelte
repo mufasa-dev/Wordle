615dc4564ed0ce59a16fc86afad158ad9b445b4a
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-26 20:15:32 -0300

    feat: start component keyboard

---
+++ src/components/Keyboard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Key from "./Key.svelte";   
+</script>
+
+<div class="grid">
+    <Key letter={"A"} />
+    <Key letter={"B"} />
+    <Key letter={"C"} />
+    <Key letter={"D"} />
+    <Key letter={"E"} />
+</div>
