e95f3d38c5191090d7324819ded011993d9472c3
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-25 22:25:34 -0300

    fix: ajust import of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,svelte}"],
+  content: ["./index.html", "./src/**/*.{html,js,ts,svelte}"],
   theme: {
     extend: {},
   },
