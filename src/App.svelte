faed3bc3c880caded7ab16d4b1f95abf53fdbd84
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-25 22:25:57 -0300

    feat: add style to the component

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
-  import './styles/TailwindCSS.svelte'
   import { getRandomWord } from './game/words';
 
-  let palavraSecreta: string;
+  let word: string;
   let start: boolean = false;
 
-
   function  startGame() {
-    palavraSecreta = getRandomWord();
+    word = getRandomWord();
+    console.log('palavra', word)
+    start = true;
   }
 </script>
 
 <main>
-  <button>
-    Play
-  </button>
+  <div class="w-full h-screen flex items-center justify-center bg-gray-500">
+    {#if start}
+      <div>
+        <b>Palavra secreta: {word}</b>
+      </div>
+    {:else}
+      <div class="text-center">
+        <h1>Wordle</h1>
+        <h2>Get 6 chances to guess a 5-letter word.</h2>
+        <button on:click={startGame} class="bg-blue-900 text-white rounded px-4 py-2 hover:bg-blue-950">
+          Play
+        </button>
+      </div>
+    {/if}
+  </div>
+  
 </main>
