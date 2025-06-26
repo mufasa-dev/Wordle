ed7ced82ba41e0116ad9099aebb18370e5ffe93d
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-26 20:16:14 -0300

    feat: import board on app

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { getRandomWord } from './game/words';
   import Title from './components/Title.svelte';
+  import Board from './components/Board.svelte';
 
   let word: string;
   let start: boolean = false;
@@ -15,9 +16,7 @@
 <main>
   <div class="w-full h-screen flex items-center justify-center bg-gray-500">
     {#if start}
-      <div>
-        <b class="text-blue">Palavra secreta: {word}</b>
-      </div>
+      <Board word={word} />
     {:else}
       <Title onStart={startGame} />
     {/if}
