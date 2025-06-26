33e878fa05b136dcff3df04b04840672cb1b5661
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-25 22:48:32 -0300

    feat: add component title

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { getRandomWord } from './game/words';
+  import Title from './components/Title.svelte';
 
   let word: string;
   let start: boolean = false;
@@ -15,16 +16,10 @@
   <div class="w-full h-screen flex items-center justify-center bg-gray-500">
     {#if start}
       <div>
-        <b>Palavra secreta: {word}</b>
+        <b class="text-blue">Palavra secreta: {word}</b>
       </div>
     {:else}
-      <div class="text-center">
-        <h1>Wordle</h1>
-        <h2>Get 6 chances to guess a 5-letter word.</h2>
-        <button on:click={startGame} class="bg-blue-900 text-white rounded px-4 py-2 hover:bg-blue-950">
-          Play
-        </button>
-      </div>
+      <Title onStart={startGame} />
     {/if}
   </div>
   
---
+++ src/components/Title.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let onStart: () => void;
+</script>
+
+<div class="text-center">
+    <h1>Wordle</h1>
+    <h2>Get 6 chances to guess a 5-letter word.</h2>
+    <button on:click={onStart} class="bg-blue-900 text-white rounded px-4 py-2 hover:bg-blue-950">
+        Play
+    </button>
+</div>
