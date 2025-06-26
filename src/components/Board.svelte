fe643d42c89f7d826c0a05ea0b9c7c6633cabf92
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-26 20:15:59 -0300

    feat: create board

---
+++ src/components/Board.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Tile from "./Tile.svelte";
+    import Keyboard from "./Keyboard.svelte";
+    
+    export let word: string = "";
+
+    let [l1, l2, l3, l4, l5] = word;
+</script>
+
+<div class="flex">
+    <Tile letter={l1} />
+    <Tile letter={l2} />
+    <Tile letter={l3} />
+    <Tile letter={l4} />
+    <Tile letter={l5} />
+</div>
+
+<Keyboard />
