d13037d40f9f7c66c5d64ba332819ff621fc861d
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-27 16:53:34 -0300

    feat: ajust app svelt imports

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,14 @@
   import { getRandomWord } from './game/words';
   import Title from './components/Title.svelte';
   import Board from './components/Board.svelte';
+    import Keyboard from './components/Keyboard.svelte';
 
   let word: string;
   let start: boolean = false;
+
+  const maxAttempts = 5;
+  let actualAtempt: string = "";
+  let attempts: string[] = [];
 
   function  startGame() {
     word = getRandomWord();
@@ -16,7 +21,11 @@
 <main>
   <div class="w-full h-screen flex items-center justify-center bg-gray-500">
     {#if start}
-      <Board word={word} />
+      {#each attempts as attempt}
+        <Board secretWork={word} word={attempt} />
+      {/each}
+
+      <Keyboard />
     {:else}
       <Title onStart={startGame} />
     {/if}
