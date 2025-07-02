09c5ee67161cabfe8649d670a5bd1669106fedb9
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-01 23:09:35 -0300

    feat: ajust title

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="text-center">
-    <h1>Wordle</h1>
-    <h2>Get 6 chances to guess a 5-letter word.</h2>
-    <button on:click={onStart} class="bg-blue-900 text-white rounded px-4 py-2 hover:bg-blue-950">
+    <h1 class="text-center text-white text-5xl font-bold">Wordle</h1>
+    <h2 class="text-white text-2xl my-1">Get 6 chances to guess a 5-letter word.</h2>
+    <button on:click={onStart} class="bg-blue-900 text-white rounded-full mt-2 px-10 py-2 hover:bg-blue-950">
         Play
     </button>
 </div>
