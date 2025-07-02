c359eb45422780650743ef5c359ff0875d30fcd1
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-02 19:15:44 -0300

    feat: create game component

---
+++ src/components/Game.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { getRandomWord } from '../game/words';
+  import Title from './Title.svelte';
+  import Board from './Board.svelte';
+  import Keyboard from './Keyboard.svelte';
+
+  let word: string;
+  let start: boolean = false;
+  let selectedIndex: number = 0;
+
+  const maxAttempts = 5;
+  let actualAtempt: number = 0;
+  let attempts: string[] = [
+    "_____",
+    "_____",
+    "_____",
+    "_____",
+    "_____"
+  ];
+
+  function  startGame() {
+    word = getRandomWord();
+    console.log('palavra', word)
+    start = true;
+  }
+
+  function selectIndex(index: number) {
+    selectedIndex = index;
+  }
+</script>
+
+<main>
+  <div class="w-full h-screen flex flex-col items-center justify-center bg-gray-500">
+    {#if start}
+      <h1 class="text-center text-white text-5xl font-bold mb-2">Wordle</h1>
+      {#each attempts as attempt, i}
+        <Board secretWork={word} word={attempt} selectedIndex={selectedIndex} selectIndex={selectIndex} isActual={actualAtempt == i} />
+      {/each}
+
+      <Keyboard />
+    {:else}
+      <Title onStart={startGame} />
+    {/if}
+  </div>
+  
+</main>
