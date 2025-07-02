c1e0fbd7678cfbf24ae8890cdb0157c0b2799796
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-02 19:29:50 -0300

    feat: remove letter component

--- src/components/Key.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    export let letter: string = "";
-    export let status: string = "";
-</script>
-
-<div class="p-2 bg-blue-200 rounded text-center cursor-pointer hover:bg-blue-400">
-    {letter}
-</div>
