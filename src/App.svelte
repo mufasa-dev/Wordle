105a28a13b83a4c26444f834543803403a69a449
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-02 19:13:27 -0300

    feat: ajust app component

--- src/App.svelte
+++ src/App.svelte
@@ -6,23 +6,35 @@
 
   let word: string;
   let start: boolean = false;
+  let selectedIndex: number = 0;
 
   const maxAttempts = 5;
-  let actualAtempt: string = "";
-  let attempts: string[] = [];
+  let actualAtempt: number = 0;
+  let attempts: string[] = [
+    "_____",
+    "_____",
+    "_____",
+    "_____",
+    "_____"
+  ];
 
   function  startGame() {
     word = getRandomWord();
     console.log('palavra', word)
     start = true;
   }
+
+  function selectIndex(index: number) {
+    selectedIndex = index;
+  }
 </script>
 
 <main>
-  <div class="w-full h-screen flex items-center justify-center bg-gray-500">
+  <div class="w-full h-screen flex flex-col items-center justify-center bg-gray-500">
     {#if start}
-      {#each attempts as attempt}
-        <Board secretWork={word} word={attempt} />
+      <h1 class="text-center text-white text-5xl font-bold mb-2">Wordle</h1>
+      {#each attempts as attempt, i}
+        <Board secretWork={word} word={attempt} selectedIndex={selectedIndex} selectIndex={selectIndex} isActual={actualAtempt == i} />
       {/each}
 
       <Keyboard />
