425b1b7897bff4f98bfeb3d681449142088e575a
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-03 18:09:35 -0300

    feat: use function to validate key to add color

--- src/components/Keyboard.svelte
+++ src/components/Keyboard.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
+    import { EnumLetters, validateKey, attempts, word } from "../game/gameManager";
+
     export let addLetter: (letter: string) => void;
     
     let letters: string[] = [
         "A", "B", "C", "D", "E", "F", "G", "H","I","J", "K", "L", "M", "N",
         "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ç", "Enter", "Del"
-    ]
+    ];
+
+    let keyColors: Record<string, string> = {}
+
+    $: {
+        const _ = $attempts + $word; // só para forçar dependência
+        const newColor: Record<string, string> = {};
+        for (const letter of letters) {
+        const status = validateKey(letter);
+        if (status == EnumLetters.NotFound)
+            newColor[letter] = "bg-blue-200 hover:bg-blue-400 opacity-40";
+        else if (status == EnumLetters.Correct)
+            newColor[letter] = "bg-green-200 hover:bg-green-400";
+        else if (status == EnumLetters.InAnotherPlace)
+            newColor[letter] = "bg-yellow-200 hover:bg-yellow-400";
+        else
+            newColor[letter] = "bg-blue-200 hover:bg-blue-400";
+        }
+        keyColors = newColor;
+    }
 </script>
 
 <div class="grid grid-cols-10 gap-1 ">
     {#each letters as letter}
-        <button class="p-2 bg-blue-200 rounded text-center cursor-pointer hover:bg-blue-400"
+        <button class="p-2 rounded text-center cursor-pointer {keyColors[letter]}"
             on:click={() => addLetter(letter)}>
             {letter}
         </button>
