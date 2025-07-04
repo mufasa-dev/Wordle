899e7fbf8d93753fdf2b14b7979d5cf9ac80c549
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-04 19:39:29 -0300

    feat: install git pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+const base = '/wordle/'
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/wordle/',
   test: {
     globals: true,
     environment: 'jsdom',
