948c1cb3412da63359a374eef8895e70e78165ff
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-05 11:07:58 -0300

    feat: add traslate file

---
+++ src/i18n.ts
@@ -0,0 +1,20 @@
+export const translations = {
+  en: {
+    title: "Wordle",
+    subtitle: "Get 6 chances to guess a 5-letter word.",
+    play: "Play",
+    success: "You won!",
+    fail: "Better luck next time!",
+    tryAgain: "Try again",
+    theWordWas: "The word was"
+  },
+  pt: {
+    title: "Termo",
+    subtitle: "Você tem 6 tentativas para adivinhar a palavra de 5 letras.",
+    play: "Jogar",
+    success: "Você venceu!",
+    fail: "Você fracassou!",
+    tryAgain: "Jogar novamente",
+    theWordWas: "A palavra era"
+  }
+};
